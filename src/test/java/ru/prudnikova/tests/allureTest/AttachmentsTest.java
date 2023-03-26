6e9136feeaefe4b0ef1fb3bff8b0f7e68f4df45d
Ekaterina Prudnikova <rvsn0913@mail.ru>
2023-03-26 21:18:18 +0300

    Allure ci

---
+++ src/test/java/ru/prudnikova/tests/allureTest/AttachmentsTest.java
@@ -0,0 +1,32 @@
+package ru.prudnikova.tests.allureTest;
+
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.Test;
+import ru.prudnikova.pages.GitHubPages;
+
+import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.webdriver;
+import static io.qameta.allure.Allure.attachment;
+import static io.qameta.allure.Allure.step;
+
+public class AttachmentsTest {
+    @Test
+    public void testLambdaAttachments() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+
+        step("Открываем главную страницу", () -> {
+            open("https://github.com");
+            attachment("Source", webdriver().driver().source());
+        });
+    }
+
+    @Test
+    public void testAnnotatedAttachments() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+        GitHubPages steps = new GitHubPages();
+
+        steps.openMainPage();
+        steps.takeScreenshot();
+    }
+}
