1b9c6c3607d4057ab5d7849253473ba08decd539
Ekaterina Prudnikova <rvsn0913@mail.ru>
2023-03-26 21:19:42 +0300

    Allure ci

--- src/test/java/ru/prudnikova/tests/allureTest/AttachmentsTest.java
+++ src/test/java/ru/prudnikova/tests/allureTest/AttachmentsTest.java
@@ -15,7 +15,7 @@ public class AttachmentsTest {
     public void testLambdaAttachments() {
         SelenideLogger.addListener("allure", new AllureSelenide());
 
-        step("Открываем главную страницу", () -> {
+        step("Открываем главную страницуe", () -> {
             open("https://github.com");
             attachment("Source", webdriver().driver().source());
         });
